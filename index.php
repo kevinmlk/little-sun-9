56ce1b525a1c313076effdc66fe2c5b990609120
Kevin Malekera <r0900654@student.thomasmore.be>
2024-03-29 14:42:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Links CSS -->
+  <link rel="stylesheet" href="./assets/css/reset.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <!-- Title -->
+  <title>Little Sun</title>
+</head>
+<body>
+  <main>
+    <h1>Little Sun</h1>
+
+  </main>
+  <!-- Links JS -->
+  <script src="./assets/js/app.js" ></script>
+</body>
+</html>
