3627ad89ad2941671fe7b7fb4016c2fd3ea85371
Kevin Malekera <r0900654@student.thomasmore.be>
2024-04-08 19:27:55 +0200

    Added form with input fields and some theme styling for the login page

--- login.php
+++ login.php
@@ -3,9 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Links CSS -->
+  <link rel="stylesheet" href="./assets/css/reset.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
   <title>Shiftplanner - Login</title>
 </head>
 <body>
-  <h1>Login</h1>
+  <main class="bg-image">
+    <section>
+      <h1>Login</h1>
+      <p>Use your Little Sun account to login</p>
+      <!-- Login form -->
+      <form action="">
+        <input type="text" name="email-phone" placeholder="Email or phone">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Login" class="btn btn-primary">
+      </form>
+    </section>
+
+  </main>
 </body>
 </html>
