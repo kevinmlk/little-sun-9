0c7f0e190a30386ba70ba31d393fca147e246bd8
Kevin Malekera <r0900654@student.thomasmore.be>
2024-04-20 17:36:19 +0200

    Removed current design style and added bootstrap with some text context

--- login.php
+++ login.php
@@ -27,14 +27,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Links CSS -->
   <link rel="stylesheet" href="./assets/css/reset.css">
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/css/style.css">
-  <title>Login | Little Sun</title>
+  <!-- Tab Title -->
+  <title>Login | Little Sun Shiftplanner</title>
 </head>
 <body>
-  <main class="bg-image">
-    <section class="card-login">
-      <h1>Login</h1>
-      <p>Use your Little Sun account to login</p>
+  <main class="container d-flex">
+    <!-- Intro Text -->
+    <section>
+      <h1>Little Sun Shifplanner</h1>
+      <p>
+        Welcome to Little Sun Shiftplanner, the ultimate platform fro shift planner in Zambia!
+        At Little Sun Shiftplanner, we empower workers to take control of their schedules by
+        defining their roles and selecting preferred work locations. Our user-friendly interface
+        allows workers to plan their availability for shifts and even schedule well-deserved
+        vacations with ease.
+      </p>
+    </section>
+
+    <!-- Login Form -->
+    <section class="card">
+      <h1>Welcome</h1>
       <!-- Login error message -->
 				<?php	if (isset($error)): ?>
 				<div class="form-error">
@@ -43,26 +57,24 @@
 					</p>
 				</div>
 				<?php endif; ?>
+        <!-- Login form -->
+        <form action="login.php" method="post" class="card">
+          <div class="form-field">
+            <label for="email">Email</label>
+            <input class="input-field" type="text" name="email" placeholder="Email or phone">
+            <label for="password">Password</label>
+            <input class="input-field" type="password" name="password" placeholder="Password">
+          </div>
 
-      <!-- Login form -->
-      <form action="login.php" method="post" class="login-form">
-        <div class="form-field">
-          <input class="input-field" type="text" name="email" placeholder="Email or phone">
-          <input class="input-field" type="password" name="password" placeholder="Password">
-        </div>
-
-        <input type="submit" value="Login" class="btn btn-primary">
-      </form>
-
-      <div id="login-links-container">
-        <ul>
-          <li><a href="">Help</a></li>
-          <li><a href="">Privacy</a></li>
-          <li><a href="">Terms</a></li>
-        </ul>
-      </div>
+          <input type="submit" value="Login" class="btn btn-primary">
+        </form>
+        <!-- Extra links -->
+      <ul>
+        <li><a href="">Forgot password?</a></li>
+        <li><a href="">Don't have an account?</a></li>
+      </ul>
+      
     </section>
-
   </main>
 </body>
 </html>
