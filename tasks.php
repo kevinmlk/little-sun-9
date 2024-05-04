71531f7c9a8e0bd945e2c875faaf9693ebd64243
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-04 16:22:51 +0200

    Added a tasks overview page

--- tasks.php
+++ tasks.php
@@ -1,12 +1,17 @@
 <?php
+  // Include bootstrap
   include_once(__DIR__ . '/bootstrap.php');
 
-  // Check if the user is logged in
   session_start();
-  if (!isset($_SESSION['loggedin'])) {
-    header('Location: login.php');
+
+  if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'Admin') {
+    // Redirect user to login page or show an error message
+    header("Location: index.php");
+    exit;
   }
 
+  // Toon alle gebruikers
+  $tasks = Task::getAllTaskTypes();
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,8 +21,7 @@
   <link rel="stylesheet" href="./assets/css/reset.css">
   <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/css/style.css">
-  <!-- Title -->
-  <title>Little Sun</title>
+  <title>Tasks | Little Sun Shiftplanner</title>
 </head>
 <body>
   <!-- Start Navbar -->
@@ -33,12 +37,9 @@
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-          <?php if (!empty($_SESSION['profile-picture'])): ?>
-          <img src="./images/profile/<?php echo $_SESSION['profile-picture']; ?>" id="img-navbar" class="h-50" alt="<?php echo $_SESSION['name']; ?> profile picture">
-          <?php endif; ?>
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 mb-5">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Dashboard</a>
+              <a class="nav-link" href="index.php">Dashboard</a>
             </li>
             <hr>
             <?php if ($_SESSION['role'] === 'Admin'): ?>
@@ -46,27 +47,22 @@
               <a class="nav-link" href="users.php">Users Overview</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="hubs.php">Hub Overview</a>
+              <a class="nav-link active" aria-current="page" href="#">Hubs Overview</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="tasks.php">Task Overview</a>
-            </li>
-            <hr>
             <?php endif; ?>
+            <hr>
             <li class="nav-item">
               <a class="nav-link" href="#">Calendar</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Time Tracker</a>
             </li>
-            <hr>
             <li class="nav-item">
               <a class="nav-link" href="#">Shiftplan</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Shiftswap</a>
             </li>
-            <hr>
             <li class="nav-item">
               <a class="nav-link" href="#">Working hours</a>
             </li>
@@ -79,12 +75,36 @@
       </div>
     </div>
   </nav>
-  <!-- Start main content -->
-  <main class="container mt-5 pt-5">
-    <h1>Welcome back <?php echo $_SESSION['name']; ?>!</h1>
-    <h2>Dashboard</h2>
-    
+
+<!-- Main Content -->
+<main class="container pt-5">
+    <!-- Add Hub Section -->
+    <section class="mt-5">
+      <h1 class="mb-3">Tasks</h1>
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h2>Tasks overview</h2>
+        <a href="create-task.php" class="btn btn-primary">Add new task type</a>
+      </div>
+
+      <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col"><strong>Id</strong></th>
+            <th scope="col"><strong>Task type name</strong></th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($tasks as $key => $task): ?>
+            <tr>
+              <th scope="row"><a href="task-details.php?id=<?php echo $key; ?>"><?php echo $task['Id']; ?></a></th>
+              <td><?php echo $task['taskname']; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </section>
   </main>
+  
   <!-- Links JS -->
   <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
   <script src="./assets/js/app.js" ></script>
