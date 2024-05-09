addad2642e9d465485364c06091a110aa05e9b2f
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-09 20:07:13 +0200

    Successfully display the shift task of the user on the calendar

--- test.php
+++ test.php
@@ -1,19 +1,36 @@
 <?php
 
-  $data = [
-    [
-      'title' => 'event1',
-      'start' => '2024-05-01',
-    ],
-    [
-      'title' => 'event2',
-      'start' => '2024-05-05',
-      'end' => '2024-05-07',
-    ],
-    [
-      'title' => 'event3',
-      'start' => '2024-05-09T12:30:00',
-      'allDay' => false,
-    ],
-  ];
+  include_once(__DIR__ . '/classes/Shift.php');
+
+  $allShifts = Shift::getAllShifts();
+
+  $data = [];
+
+  foreach ($allShifts as $shift) {
+    $startTime = (new DateTime($shift['StartTime']))->format('Y-m-d\TH:i:s');
+    $endTime = (new DateTime($shift['EndTime']))->format('Y-m-d\TH:i:s');
+
+    $data[] = [
+      'title' => $shift['Taskname'],
+      'start' => $startTime,
+      'end' => $endTime,
+    ];
+  }
+
+  // $data = [
+  //   [
+  //     'title' => 'event1',
+  //     'start' => '2024-05-01',
+  //   ],
+  //   [
+  //     'title' => 'event2',
+  //     'start' => '2024-05-05',
+  //     'end' => '2024-05-07',
+  //   ],
+  //   [
+  //     'title' => 'event3',
+  //     'start' => '2024-05-09T12:30:00',
+  //     'allDay' => false,
+  //   ],
+  // ];
   echo json_encode($data);
