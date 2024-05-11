a37a27edd88d3512a1075ca69777d912220f6345
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-11 21:55:53 +0200

    Updated db export

--- little_sun.sql
+++ little_sun.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2024 at 01:30 PM
+-- Generation Time: May 11, 2024 at 09:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -104,8 +104,13 @@ CREATE TABLE `shifts` (
 --
 
 INSERT INTO `shifts` (`Id`, `StartTime`, `EndTime`, `EmployeeId`, `TaskId`, `LocationId`) VALUES
-(1, '2024-05-05 19:48:54', '2024-05-06 15:30:00', 3, 1, 1),
-(2, '2024-05-05 19:49:00', '2024-05-06 15:30:00', 3, 1, 1);
+(1, '2024-05-13 07:00:00', '2024-05-13 15:30:00', 3, 2, 1),
+(2, '2024-05-14 08:00:00', '2024-05-14 14:00:00', 6, 3, 2),
+(11, '2024-05-14 07:00:00', '2024-05-14 15:30:30', 17, 2, 2),
+(12, '2024-05-16 07:00:00', '2024-05-16 15:30:00', 20, 2, 3),
+(13, '2024-05-17 12:00:00', '2024-05-17 16:00:00', 19, 3, 3),
+(14, '2024-05-20 08:00:00', '2024-05-20 10:00:00', 16, 3, 1),
+(15, '2024-05-21 12:00:00', '2024-05-21 14:00:00', 16, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -136,11 +141,9 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`Id`, `Taskname`) VALUES
-(1, 'Office cleaning'),
-(2, 'Office duty'),
-(3, 'Office cleaning'),
-(4, 'Office Duty 2'),
-(5, 'Office cleaning 3');
+(1, 'no task assigned'),
+(2, 'office duty'),
+(3, 'office cleaning');
 
 -- --------------------------------------------------------
 
@@ -182,17 +185,10 @@ INSERT INTO `users` (`Id`, `Firstname`, `Lastname`, `Email`, `Password`, `Profil
 (5, 'Jane', 'Doe', 'jane.doe@littlesun.zm', '$2y$15$BN6aS7ryZ1SHJUjcZ9OiruttGZ1y3h6TErJ9TVxFTtHp0JFn.PWC.', '', 2),
 (6, 'Jeremy', 'Decuypere', 'jeremy.decuypere@littlesun.zm', '$2y$15$H9Sm.Yfdi8AJ1KDfP0upQuB31.z7BCteNOW8k/kLEKfYzFYxurx4i', '', 1),
 (7, 'Jennifer', 'Doe', 'jennifer.doe@littlesun.zm', '$2y$15$HThqfT7KJYnmMFz.9ogNq.D/mdZoaK2yT2KgOCLCXK9WLkwIts2Za', '', 1),
-(16, 'Frank', 'Ribery', 'frank.ribery@littlesun.zm', '$2y$15$spxP8saS5Bu5pNPYDSfH/ucmViCpgsv4JWb6XY1TPeTWj2ZeZw4.C', 'Frank-Ribery-pexels-tony-schnagl-5588224.jpg', 3),
-(17, 'Amanda', 'Seales', 'amanda.seales@littlesun.zm', '$2y$15$vQlvrpWq4vsszevSpAg47.2Kd0w1kOMxCJO7.i4XOF8Uf05OnC6Ga', 'Amanda-Seales-pexels-olly-3769021.jpg', 3),
-(19, 'Kirk', 'Leoon', 'kirk.leoon@littlesun.zm', '$2y$15$KIO/WfUKBgnNFLvSgU78neBcXaonvEWCVVXJhRlxOf2B1VX1/jZ5.', 'Kirk-Leoon-pexels-tony-schnagl-5588224.jpg', 1),
-(20, 'Dean', 'Saber', 'dean.saber@littlesun.zm', '$2y$15$CXC01H3GyFirHzyZVTZwbuNXMSOYrVeFsFl1K9yCDdz/6fffLsisO', 'Dean-Saber-pexels-olly-3769021.jpg', 1),
-(21, 'king', 'kong', 'king.kong@littlesun.zm', '$2y$15$54hm9Ex0XgFnEOJcj9z81ua/lE7baBq3QZ05YuLBuDwEx1sX8.SEy', 'king-kong-pexels-rada-aslanova-150604297-22626143.jpg', 1),
-(22, 'test', 'test', 'test@test.com', '$2y$15$FOHE391Ssq3O4HNqEEl7yOQrVsJy6vV6JFjHGByKMK21PTj0/9t4.', 'test-test-pexels-tony-schnagl-5588224.jpg', 1),
-(23, 'test', 'test', 'test@test.com', '$2y$15$RgZb59.UnWdUwKf7eIYh6.mzeDUR74RF.QfSqLI8a1Rjh7Lgjj5q2', 'test-test-pexels-tony-schnagl-5588224.jpg', 1),
-(24, 'test2', 'test2', 'test2@test.com', '$2y$15$YXWZhf6n.dxwVrK5RhgPjOo82BhPH.iGJsP/Vd0SmL27j2yQGn2Ai', 'test2-test2-pexels-olly-3769021.jpg', 1),
-(25, 'test5', 'test5', 'test5@test5.com', '$2y$15$NmOxcODI2i6HXvRpzh75Veuu8RKPyv3UBenCJUnYzFzFcSRylRJ6y', 'test5-test5-pexels-olly-3769021.jpg', 1),
-(26, 'test5', 'test5', 'test5@test5.com', '$2y$15$BE02WvbUzfmXzxVdNMK.COXXQdkd3PJQhaOtC2CUVkfGFxVrQoX42', 'test5-test5-pexels-olly-3769021.jpg', 1),
-(27, 'Test 6', 'Test', 'test6@littlesun.zm', '$2y$15$ViR3IR/SCcemwDQ2Mj.gpOcOA/zp.svURrdV/xCOOdaW.M5SHNtEu', 'Test 6-Test-pexels-tony-schnagl-5588224.jpg', 1);
+(16, 'Frank', 'Ribery', 'frank.ribery@littlesun.zm', '$2y$15$spxP8saS5Bu5pNPYDSfH/ucmViCpgsv4JWb6XY1TPeTWj2ZeZw4.C', 'Frank-Ribery-pexels-tony-schnagl-5588224.jpg', 1),
+(17, 'Amanda', 'Seales', 'amanda.seales@littlesun.zm', '$2y$15$vQlvrpWq4vsszevSpAg47.2Kd0w1kOMxCJO7.i4XOF8Uf05OnC6Ga', 'Amanda-Seales-pexels-olly-3769021.jpg', 1),
+(19, 'Kirk', 'Leon', 'kirk.leoon@littlesun.zm', '$2y$15$KIO/WfUKBgnNFLvSgU78neBcXaonvEWCVVXJhRlxOf2B1VX1/jZ5.', 'Kirk-Leoon-pexels-tony-schnagl-5588224.jpg', 1),
+(20, 'Dean', 'Saber', 'dean.saber@littlesun.zm', '$2y$15$CXC01H3GyFirHzyZVTZwbuNXMSOYrVeFsFl1K9yCDdz/6fffLsisO', 'Dean-Saber-pexels-olly-3769021.jpg', 1);
 
 --
 -- Indexes for dumped tables
@@ -275,7 +271,7 @@ ALTER TABLE `schedules`
 -- AUTO_INCREMENT for table `shifts`
 --
 ALTER TABLE `shifts`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `shiftswap`
@@ -299,7 +295,7 @@ ALTER TABLE `timeoffrequests`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
