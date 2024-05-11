c64aaec79294af20681695226564d8157ff8f977
Rocky Lai <r0912510@student.thomasmore.be>
2024-05-11 23:35:05 +0200

    Update checkOut.php

--- checkOut.php
+++ checkOut.php
@@ -1,15 +1,57 @@
+<?php
+//button to check-in[x]
+//check if the button is clicked[x]
+//if the button is clicked, echo "Check-in successful"[x]
+//connect to the database[x]
+//create a connection[x]
+//check if the connection is successful[x]
+//if the connection is successful, echo "Connected successfully[x]"
+//if click on the button, insert a new row in the database with the current date and time[]
+
+
+if(isset($_POST['checkOut'])){
+    echo "Check-in successful";
+}
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "little_sun1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$currentDateTime = date('Y-m-d H:i:s');
+$sql = "INSERT INTO workingHours (check_out_time) VALUES ('$currentDateTime')";
+
+ if ($conn->query($sql) === TRUE) {
+        echo "Check-out successful";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-   
+    <title>Test</title>
 </head>
 <body>
-    </div>
-  </div>
-</div>
+    <h1>Check-out</h1>
+    <p>Check-our to your shift</p>
+    <form action="checkOut.php" method="POST">
+        <label for="checkOut">Check-out</label>
+        <input type="submit" name="checkOut" value="Check-out">
+
+    </form>
 </body>
 </html>
