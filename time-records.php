894a6a5bee5c4ee8df598fbfbf1d49249ce7c4a4
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-21 08:11:38 +0200

    Removed shiftswap and modified vacation.php to time-off.php

--- time-records.php
+++ time-records.php
@@ -164,7 +164,7 @@
     <section id="overtime-section" class="mt-5">
       <div class="d-flex justify-content-between align-items-center mb-3">
         <h2>Overtime</h2>
-        <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#filterOvertimeModal"><i class="bi bi-funnel me-2"></i>Filter employees</button>
+        <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#overtimeModal"><i class="bi bi-funnel me-2"></i>Extra options</button>
       </div>
 
       <table class="table table-striped table-hover">
@@ -188,47 +188,30 @@
         </tbody>
       </table>
 
-      <!-- filterOvertimeModal -->
-      <div class="modal fade" id="filterOvertimeModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="filterOvertimeModal" aria-hidden="true">
+      <!-- overtimeModalM -->
+      <div class="modal fade" id="overtimeModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="overtimeModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="staticBackdropLabel">Filter employee</h1>
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">Add shift</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <form action="./includes/add-shift.inc.php" method="post">
+              <form action="" method="post">
                 <!-- Hub Select -->
                 <div class="mb-3">
-                  <label for="hub-select" class="col-form-label">Choose hub:</label>
+                  <label for="hub-select" class="col-form-label">Choose employee:</label>
                   <select name="hub-select" class="form-select" aria-label="Hub select" id="hub-select" required>
-                    <?php foreach($locations as $l): ?>
-                      <option value="<?php echo $l['Id']; ?>"><?php echo $l['Hubname'];?> (<?php echo $l['Hublocation']; ?>)</option>
+                    <?php foreach($clockedShifts as $cs): ?>
+                      <option value="<?php echo $cs['Id']; ?>"><?php echo $cs['Firstname']; ?> <?php echo $cs['Lastname']; ?></option>
                     <?php endforeach; ?>
                   </select>
                 </div>
-                <!-- Employee Select -->
+
                 <div class="mb-3">
-                  <label for="employee-select" class="col-form-label">Choose an employee from hub:</label>
-                  <select name="employee-select" class="form-select" id="employee-select" aria-label="Employee select" required>
-                  </select>
+                      <label for="month-overtime" class="col-form-label">Overtime </label>
                 </div>
-                <!-- Task -->
-                <div class="mb-3">
-                  <label for="task-select" class="col-form-label">Assign task:</label>
-                  <select name="task-select" class="form-select" id="task-select" aria-label="Task select" required>
-                  </select>
-                </div>
-                <!-- Start Shift -->
-                <div class="mb-3">
-                  <label for="start-time" class="col-form-label">Start shift:</label>
-                  <input name="start-time" id="start-time" class="form-control" min="<?php echo date("Y-m-d\TH:i"); ?>" type="datetime-local" required>
-                </div>
-                <!-- End Shift -->
-                <div class="mb-3">
-                  <label for="end-time" class="col-form-label">End shift:</label>
-                  <input name="end-time" class="form-control" min="<?php echo date("Y-m-d\TH:i"); ?>" type="datetime-local" required>
-                </div>
+
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                   <button type="submit" class="btn btn-primary">Add new shift</button>
